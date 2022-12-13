ca0d1636157a2bfeeacd820f4ebaf423e90886de
Volkan Sahin <vsahin0985@gmail.com>
2022-12-13 14:05:02 +0300

    Create circular_drop_down.dart

---
+++ lib/view/widgets/circular_drop_down.dart
@@ -0,0 +1,47 @@
+class CircularDropDownMenu extends StatefulWidget {
+  final String hintText;
+  final List<DropdownMenuItem> dropDownMenuItem;
+  final dynamic onChanged;
+  CircularDropDownMenu({
+    required this.onChanged,
+    required this.dropDownMenuItem,
+    required this.hintText,
+  });
+  @override
+  _CircularDropDownMenuState createState() => _CircularDropDownMenuState();
+}
+
+class _CircularDropDownMenuState extends State<CircularDropDownMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 50,
+      margin: EdgeInsets.only(top: 16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(
+          color: Colors.grey,
+          width: 1,
+        ),
+      ),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton(
+          items: widget.dropDownMenuItem,
+          onChanged: widget.onChanged,
+          hint: Container(
+            padding: EdgeInsets.only(left: 12),
+            child: Text(
+              widget.hintText,
+              style: TextStyle(
+                fontFamily: 'Muli',
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
