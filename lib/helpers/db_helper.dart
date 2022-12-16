d7d92733a0c9065a4f0c30641c28673950c456c3
Volkan Sahin <vsahin0985@gmail.com>
2022-12-16 15:07:53 +0300

    Create db_helper.dart

---
+++ lib/helpers/db_helper.dart
@@ -0,0 +1,28 @@
+class DBHelper{
+  static Database? _db;
+  static final int _version = 1;
+  static final String _tableName = "reminders";
+  
+  static Future<void> initDb() aysnc{
+    if(_db != null){
+      return;
+    }
+    try{
+      String _path = await getDatabasesPath() + 'reminder.db';
+      _db = await openDatabase(
+        _path,
+        version: _version,
+        onCreate: (db, version){
+          print("creating a new one");
+          return db.execute(
+            "CREATE TABLE $_tableName("
+            "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+            "title STRING, reminder TEXT, date STRING, "
+            "time STRING, remindBefore STRING, repeat STRING, "
+            "color STRING, isReminded STRING )",
+          ); 
+        },
+      );
+    } catch(e){print(e);}
+  }
+}
